3b9b3a02d842e3bd096fc2749490db299dfc93d5
Karol594 <karagodlewski@gmail.com>
2025-10-03 08:29:10 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <header className="bg-blue-600 text-white p-4">
+        <h1 className="text-2xl font-bold">KarakalpakVoice.org</h1>
+      </header>
+      <main className="container mx-auto p-4">
+        <p className="text-lg">Сәлем! Сайт қурылыста...</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
