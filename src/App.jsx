ca4c34fde06a2d6141e333d249fd1d854aaa51b9
Karol594 <karagodlewski@gmail.com>
2025-11-24 23:50:30 +0100

    Fix import path for Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Hero from "./components/Hhero";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <main className="min-h-screen w-full">
+    <div className="min-h-screen bg-gray-900 text-white">
       <Hero />
-    </main>
+    </div>
   );
 }
 
