b3a2cc24d8aab0a35996da93a487bb7094e772bb
Karol594 <karagodlewski@gmail.com>
2025-12-03 22:23:20 +0100

    Add routing with BrowserRouter to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <main className="w-full min-h-screen">
-      <Home />
-    </main>
+    <BrowserRouter>
+      <main className="w-full min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
