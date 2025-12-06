33b58660f058a22fdf096ffe12a78548dee189c8
Karol594 <karagodlewski@gmail.com>
2025-12-06 15:28:57 +0100

    Reorder imports and update i18n configuration

--- src/i18n.js
+++ src/i18n.js
@@ -1,21 +1,23 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
-import en from "./locales/en.json";
-import ru from "./locales/ru.json";
-import kk from "./locales/kk.json";
-import kz from "./locales/kz.json";
+import kk from './locales/kk.json';
+import ru from './locales/ru.json';
+import en from './locales/en.json';
+import pl from './locales/pl.json';
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { translation: en },
-    ru: { translation: ru },
-    kk: { translation: kk },
-    kz: { translation: kz },
-  },
-  lng: "kk",
-  fallbackLng: "kk",
-  interpolation: { escapeValue: false },
-});
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      kk: { translation: kk },
+      ru: { translation: ru },
+      en: { translation: en },
+      pl: { translation: pl }
+    },
+    lng: "kk",
+    fallbackLng: "kk",
+    interpolation: { escapeValue: false }
+  });
 
 export default i18n;
