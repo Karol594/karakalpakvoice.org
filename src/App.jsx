d3379f803531d714da0db524392100502a0b295e
Karol594 <karagodlewski@gmail.com>
2025-12-06 16:07:41 +0100

    Refactor routing and navigation components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
+import News from "./pages/News";
+import Sport from "./pages/Sport";
+import Tradition from "./pages/Tradition";
+import Religion from "./pages/Religion";
+import History from "./pages/History";
+import Geography from "./pages/Geography";
+import People from "./pages/People";
+import Contact from "./pages/Contact";
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <Header />
+    <Router>
+      <Navigation />
 
-      <main className="pt-20 w-full min-h-screen bg-black text-white">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/sport" element={<Sport />} />
+        <Route path="/tradition" element={<Tradition />} />
+        <Route path="/religion" element={<Religion />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/geography" element={<Geography />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/contact" element={<Contact />} />
+
+        {/* Болашақ бетлерге орын қалдырылды */}
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
