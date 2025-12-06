2e7f5e698b737e4188409e1f954c325181560af8
Karol594 <karagodlewski@gmail.com>
2025-12-06 09:13:16 +0100

    Refactor i18n configuration and update languages

--- src/i18n.js
+++ src/i18n.js
@@ -1,26 +1,21 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
-// Тил ресурслары
-import ru from './locales/ru.json';
-import kk from './locales/kk.json';
-import en from './locales/en.json';
-import pl from './locales/pl.json';
+import en from "./locales/en.json";
+import ru from "./locales/ru.json";
+import kk from "./locales/kk.json";
+import kz from "./locales/kz.json";
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      ru: { translation: ru },
-      kk: { translation: kk },
-      en: { translation: en },
-      pl: { translation: pl }
-    },
-    lng: 'ru', // Дәслепки тил - РУССКИЙ
-    fallbackLng: 'ru',
-    interpolation: {
-      escapeValue: false
-    }
-  });
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    ru: { translation: ru },
+    kk: { translation: kk },
+    kz: { translation: kz },
+  },
+  lng: "kk",
+  fallbackLng: "kk",
+  interpolation: { escapeValue: false },
+});
 
 export default i18n;
