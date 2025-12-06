ef0f400a52cff3ac0133fa5e8d4dcedbfd2f7705
Karol594 <karagodlewski@gmail.com>
2025-12-06 22:10:06 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,49 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navigation from "./components/Navigation";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import About from './pages/About';
+import Bots from './pages/Bots';
+import Constitution from './pages/Constitution';
+import Contact from './pages/Contact';
+import Declaration from './pages/Declaration';
+import Geography from './pages/Geography';
+import History from './pages/History';
+import JoinUs from './pages/JoinUs';
+import News from './pages/News';
+import People from './pages/People';
+import QaraAI from './pages/QaraAI';
+import Religion from './pages/Religion';
+import Sovereignty from './pages/Sovereignty';
+import Sport from './pages/Sport';
+import Tradition from './pages/Tradition';
 
-import Home from "./pages/Home";
-import About from "./pages/About";
-import News from "./pages/News";
-import Culture from "./pages/Culture";
-import Dictionary from "./pages/Dictionary";
-import Lessons from "./pages/Lessons";
-import Media from "./pages/Media";
-import Contact from "./pages/Contact";
-
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/culture" element={<Culture />} />
-        <Route path="/dictionary" element={<Dictionary />} />
-        <Route path="/lessons" element={<Lessons />} />
-        <Route path="/media" element={<Media />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <div className="min-h-screen bg-black">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sovereignty" element={<Sovereignty />} />
+          <Route path="/declaration" element={<Declaration />} />
+          <Route path="/constitution" element={<Constitution />} />
+          <Route path="/qara-ai" element={<QaraAI />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/bots" element={<Bots />} />
+          <Route path="/geography" element={<Geography />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/join" element={<JoinUs />} />
+          <Route path="/people" element={<People />} />
+          <Route path="/religion" element={<Religion />} />
+          <Route path="/sport" element={<Sport />} />
+          <Route path="/tradition" element={<Tradition />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
+export default App;
