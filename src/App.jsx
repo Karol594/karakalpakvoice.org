8da2f6eb81d5714f9b9a0357bd2eb215e9c78c02
Karol594 <karagodlewski@gmail.com>
2025-12-09 20:19:30 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,41 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
-import ScrollToTop from './components/ScrollToTop';
-import Home from './pages/Home';
-import About from './pages/About';
-import Bots from './pages/Bots';
-import Constitution from './pages/Constitution';
-import Contact from './pages/Contact';
-import Declaration from './pages/Declaration';
-import Geography from './pages/Geography';
-import History from './pages/History';
-import JoinUs from './pages/JoinUs';
-import News from './pages/News';
-import People from './pages/People';
-import QaraAI from './pages/QaraAI';
-import Religion from './pages/Religion';
-import Sovereignty from './pages/Sovereignty';
-import Sport from './pages/Sport';
-import Tradition from './pages/Tradition';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+import Navigation from "./components/Navigation";
+
+// Беттерді импортлау
+import Home from "./pages/Home";
+import About from "./pages/About";
+import News from "./pages/News";
+import Sovereignty from "./pages/Sovereignty";
+import History from "./pages/History";
+import Geography from "./pages/Geography";
+import People from "./pages/People";
+import Tradition from "./pages/Tradition";
+import Sport from "./pages/Sport";
+import Contact from "./pages/Contact";
+
+export default function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-black">
-        <Navigation />
-        <ScrollToTop />
+      {/* Навигация */}
+      <Navigation />
+
+      {/* Контент үшін отступ (navbar fixed болғаны үшін) */}
+      <div className="pt-[90px]">  
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/sovereignty" element={<Sovereignty />} />
-          <Route path="/declaration" element={<Declaration />} />
-          <Route path="/constitution" element={<Constitution />} />
-          <Route path="/qara-ai" element={<QaraAI />} />
-          <Route path="/news" element={<News />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/bots" element={<Bots />} />
-          <Route path="/geography" element={<Geography />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/sovereignty" element={<Sovereignty />} />
           <Route path="/history" element={<History />} />
-          <Route path="/join" element={<JoinUs />} />
+          <Route path="/geography" element={<Geography />} />
           <Route path="/people" element={<People />} />
-          <Route path="/religion" element={<Religion />} />
-          <Route path="/sport" element={<Sport />} />
           <Route path="/tradition" element={<Tradition />} />
+          <Route path="/sport" element={<Sport />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </Router>
   );
 }
-
-export default App;
